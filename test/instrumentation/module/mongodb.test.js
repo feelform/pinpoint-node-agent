61e8f8be08cff2ed0910783c0bc29b9c9e957246
Yongseok <feelform@gmail.com>
2025-02-07 17:28:14 +0900

    [#265] Support MongoDB

---
+++ test/instrumentation/module/mongodb.test.js
@@ -0,0 +1,11 @@
+/**
+ * Pinpoint Node.js Agent
+ * Copyright 2020-present NAVER Corp.
+ * Apache License v2.0
+ */
+
+const test = require('tape')
+
+test('mongodb connect', (t) => {
+    t.end()
+})
