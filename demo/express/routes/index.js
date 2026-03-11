0e66c5fa0745ae2c9dde4ab408f6c4fb95a4f6c8
Yongseok <feelform@gmail.com>
2026-03-11 14:58:12 +0900

    [#455] Express Exception and URI Stats

--- demo/express/routes/index.js
+++ demo/express/routes/index.js
@@ -107,4 +107,21 @@ router.get('/api2', function (req, res, next) {
   res.status(200).json({ "result": "ok" })
 })
 
+router.get('/exception/handled', function (req, res, next) {
+  try {
+    throw new Error('Pinpoint handled exception test route')
+  } catch (error) {
+    res.status(200).json({
+      result: 'handled',
+      message: error.message
+    })
+  }
+})
+
+router.get('/exception/unhandled', function (req, res, next) {
+  const error = new Error('Pinpoint unhandled exception test route')
+  error.status = 500
+  next(error)
+})
+
 module.exports = router
